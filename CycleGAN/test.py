2b84b7852ba51977221dd286ac523f9f5c7a48da
thomas <ddeng691@gmail.com>
2022-06-12 21:50:49 +0800

    update test.py

--- CycleGAN/test.py
+++ CycleGAN/test.py
@@ -1,11 +1,11 @@
 import CycleGAN as cg
 
-content_img_path = '../dataset/tempA/*.png'
-style_img_path = '../dataset/tempB/*.png'
+content_img_path = '../dataset/A/*.png'
+style_img_path = '../dataset/B/*.png'
 output_folder = './results/'
 checkpoint_filepath = output_folder+"{epoch}_epoch/model_checkpoints/cyclegan_checkpoints.{epoch}"
 
-weight_file_path = cg.train(content_img_path, style_img_path, checkpoint_filepath, output_folder, 2)
+weight_file_path = cg.train(content_img_path, style_img_path, checkpoint_filepath, output_folder, 20)
 
 input_img_path = '../dataset/test_A/inputs/*.png'
 # weight_file_path = output_folder+"20_epoch/model_checkpoints/cyclegan_checkpoints.20"
